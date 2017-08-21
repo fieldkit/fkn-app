fc68f40afdbe54b87e9322fda1d00f092e1a5d2c
Jacob Lewallen <jlewalle@gmail.com>
2017-08-21 12:10:40 -0700

    Don't need this nothing reducer anymore.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 
-import * as nothingReducer from './nothing'
 import * as navigationReducer from './navigation'
 import * as authReducer from './auth'
 
 export default combineReducers(Object.assign(
-    nothingReducer,
     navigationReducer,
     authReducer,
 ));
