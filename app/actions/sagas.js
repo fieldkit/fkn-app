f244752915b64c278f666de06def4b9d9040483d
Jacob Lewallen <jlewalle@gmail.com>
2017-10-18 16:23:10 -0700

    Fix issue where sets would keep  downloading.
    Not using a takeLatest to look for the data set info, which would stick around.

--- app/actions/sagas.js
+++ app/actions/sagas.js
@@ -14,56 +14,6 @@ import { unixNow } from '../lib/helpers';
 
 import { navigateWelcome, navigateDeviceMenu } from './navigation';
 
-export function* downloadDataSaga() {
-    yield takeLatest(Types.DOWNLOAD_DATA_SET_START, function* watcher(action) {
-        const state = yield select();
-
-        yield put({
-            [CALL_DEVICE_API]: {
-                types: [Types.DEVICE_DATA_SET_START, Types.DEVICE_DATA_SET_SUCCESS, Types.DEVICE_DATA_SET_FAIL],
-                address: state.deviceStatus.address,
-                message: {
-                    type: QueryType.values.QUERY_DATA_SET,
-                    queryDataSet: {
-                        id: action.id
-                    }
-                }
-            },
-        });
-
-        yield takeLatest(Types.DEVICE_DATA_SET_SUCCESS, function* watcher(dataSetAction) {
-            const numberOfPages = dataSetAction.response.dataSets.dataSets[0].pages;
-
-            for (let page = 0; page < numberOfPages; ++page) {
-                yield put({
-                    [CALL_DEVICE_API]: {
-                        types: [Types.DEVICE_DOWNLOAD_DATA_SET_START, Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS, Types.DEVICE_DOWNLOAD_DATA_SET_FAIL],
-                        address: state.deviceStatus.address,
-                        message: {
-                            type: QueryType.values.QUERY_DOWNLOAD_DATA_SET,
-                            downloadDataSet: {
-                                id: action.id,
-                                page: page
-                            }
-                        }
-                    },
-                });
-
-                yield put({
-                    type: Types.DOWNLOAD_DATA_SET_PROGRESS,
-                    progress: (page / numberOfPages) * 100.0
-                });
-
-                yield take(Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS);
-            }
-
-            yield put({
-                type: Types.DOWNLOAD_DATA_SET_DONE
-            });
-        });
-    });
-}
-
 function* deviceCall(raw) {
     yield put({
         type: raw.types[0]
@@ -77,6 +27,52 @@ function* deviceCall(raw) {
         yield put(err.action)
         throw err;
     }
+}
+
+export function* downloadDataSaga() {
+    yield takeLatest(Types.DOWNLOAD_DATA_SET_START, function* watcher(action) {
+        const state = yield select();
+
+        const dataSetAction = yield call(deviceCall, {
+            types: [Types.DEVICE_DATA_SET_START, Types.DEVICE_DATA_SET_SUCCESS, Types.DEVICE_DATA_SET_FAIL],
+            address: state.deviceStatus.address,
+            message: {
+                type: QueryType.values.QUERY_DATA_SET,
+                queryDataSet: {
+                    id: action.id
+                }
+            }
+        });
+
+        const numberOfPages = dataSetAction.response.dataSets.dataSets[0].pages;
+
+        for (let page = 0; page < numberOfPages; ++page) {
+            yield put({
+                [CALL_DEVICE_API]: {
+                    types: [Types.DEVICE_DOWNLOAD_DATA_SET_START, Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS, Types.DEVICE_DOWNLOAD_DATA_SET_FAIL],
+                    address: state.deviceStatus.address,
+                    message: {
+                        type: QueryType.values.QUERY_DOWNLOAD_DATA_SET,
+                        downloadDataSet: {
+                            id: action.id,
+                            page: page
+                        }
+                    }
+                },
+            });
+
+            yield put({
+                type: Types.DOWNLOAD_DATA_SET_PROGRESS,
+                progress: (page / numberOfPages) * 100.0
+            });
+
+            yield take(Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS);
+        }
+
+        yield put({
+            type: Types.DOWNLOAD_DATA_SET_DONE
+        });
+    });
 }
 
 function* discoverDevice() {
