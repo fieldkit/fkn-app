12a4dae08c4a3c65dab23eb9e7478002549d9b17
Jacob Lewallen <jlewalle@gmail.com>
2017-10-19 09:41:59 -0700

    Use async call to download.

--- app/actions/sagas.js
+++ app/actions/sagas.js
@@ -49,26 +49,22 @@ export function* downloadDataSaga() {
         const numberOfPages = dataSetAction.response.dataSets.dataSets[0].pages;
 
         for (let page = 0; page < numberOfPages; ++page) {
-            yield put({
-                [CALL_DEVICE_API]: {
-                    types: [Types.DEVICE_DOWNLOAD_DATA_SET_START, Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS, Types.DEVICE_DOWNLOAD_DATA_SET_FAIL],
-                    address: state.deviceStatus.address,
-                    message: {
-                        type: QueryType.values.QUERY_DOWNLOAD_DATA_SET,
-                        downloadDataSet: {
-                            id: action.id,
-                            page: page
-                        }
+            yield call(deviceCall, {
+                types: [Types.DEVICE_DOWNLOAD_DATA_SET_START, Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS, Types.DEVICE_DOWNLOAD_DATA_SET_FAIL],
+                address: state.deviceStatus.address,
+                message: {
+                    type: QueryType.values.QUERY_DOWNLOAD_DATA_SET,
+                    downloadDataSet: {
+                        id: action.id,
+                        page: page
                     }
-                },
+                }
             });
 
             yield put({
                 type: Types.DOWNLOAD_DATA_SET_PROGRESS,
                 progress: (page / numberOfPages) * 100.0
             });
-
-            yield take(Types.DEVICE_DOWNLOAD_DATA_SET_SUCCESS);
         }
 
         yield put({
