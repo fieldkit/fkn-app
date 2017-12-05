df5637028d6d870fce576d3ce1ab3ffac12a9214
Jacob Lewallen <jlewallen@gmail.com>
2017-12-05 10:48:35 -0800

    Fix specs hanging due to infinite saga.

--- app/actions/discovery.js
+++ app/actions/discovery.js
@@ -8,6 +8,8 @@ import ServiceDiscovery from "react-native-service-discovery";
 import { createChannel } from './channels';
 import * as Types from './types';
 
+import Config from '../config';
+
 function createServiceDiscoveryChannel() {
     const channel = createChannel();
     const serviceDiscovery = new ServiceDiscovery();
@@ -25,12 +27,12 @@ function createServiceDiscoveryChannel() {
 }
 
 function* monitorServiceDiscoveryEvents(channel) {
-    let lastInfo = null;
-    while (true) {
-        // const info = yield call(channel.take)
-        yield put(findDeviceInfo('192.168.0.136', 12345));
-        yield delay(1000);
-        // lastInfo = info;
+    if (Config.serviceDiscoveryOnStartup) {
+        while (true) {
+            const info = yield call(channel.take)
+            // yield put(findDeviceInfo('192.168.0.136', 12345));
+            yield delay(1000);
+        }
     }
 }
 
--- app/config.js
+++ app/config.js
@@ -4,10 +4,12 @@ if (typeof(__ENV__) == 'undefined') {
 
 const configs = {
     test: {
+        serviceDiscoveryOnStartup: false,
         findDeviceTimeout: 2 * 1000,
         pingDeviceInterval: 500,
     },
     dev: {
+        serviceDiscoveryOnStartup: true,
         findDeviceTimeout: 60 * 1000,
         pingDeviceInterval: 20 * 1000,
     }
