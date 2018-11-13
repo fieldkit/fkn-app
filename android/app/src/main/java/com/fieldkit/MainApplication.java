6ef82c32dcfa2e3e2ecd95d4d7285f53496ccc5a
Jacob Lewallen <jlewallen@gmail.com>
2018-11-12 16:23:02 -0800

    Adding react-native-device-info dependency.

--- android/app/src/main/java/com/fieldkit/MainApplication.java
+++ android/app/src/main/java/com/fieldkit/MainApplication.java
@@ -3,6 +3,7 @@ package com.fieldkit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactlibrary.RNWifiPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.horcrux.svg.SvgPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNWifiPackage(),
             new UdpSocketsModule(),
             new SvgPackage(),
