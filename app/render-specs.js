70973394097a7d43301370d168985de99b1218e6
Jacob Lewallen <jlewallen@gmail.com>
2018-12-14 15:52:31 -0800

    Fixing render-specs.

--- app/render-specs.js
+++ app/render-specs.js
@@ -3,8 +3,8 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import AndroidApp, { Sagas as AndroidSagas } from '../index.android';
-import IOSApp, { Sagas as IOsSagas} from '../index.ios';
+import AndroidApp, { Sagas as AndroidSagas } from '../index';
+import IOSApp, { Sagas as IOsSagas} from '../index';
 
 describe('top level rendering', () => {
     it('renders iOS correctly', () => {
