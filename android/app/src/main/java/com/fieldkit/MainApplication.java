04a292ed7addac76956e12a435621a097b87a65a
Jacob Lewallen <jlewallen@gmail.com>
2019-05-13 10:53:07 -0700

    Ran link command to fixup some stuff in Android.

--- android/app/src/main/java/com/fieldkit/MainApplication.java
+++ android/app/src/main/java/com/fieldkit/MainApplication.java
@@ -3,6 +3,7 @@ package com.fieldkit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactlibrary.RNWifiPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNI18nPackage(),
             new SvgPackage(),
             new RNWifiPackage(),
             new RNDeviceInfo(),
